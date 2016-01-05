9028d21a171201f67eafb9ec8f73c41a1bd4d0cf
seunghwan bae <shbcaw@nate.com>
2016-01-05 16:16:49 +0900

    go

--- resource/js/caw_swipe_v0.1.js
+++ resource/js/caw_swipe_v0.1.js
@@ -1,4 +1,4 @@
-/* cancel
+/*
 	v 1.0 작업중
 		이동했을경우 클릭함수 실행 혹은 비실행 처리 해야됨 < 처리된듯
 		_this.find('img').on('dragstart',function(){ 이 함수를 네이티브로 변경해야함 << 처리된듯
@@ -11,6 +11,7 @@
 
 */
 $.fn.swipe = function( param ){
+	'use strict';
 
 	var _this = this, //object
 		factor = {
--- resource/js/common.js
+++ resource/js/common.js
@@ -1,175 +1,177 @@
-//cancel
+(function(){
+	'use strict';
 
+	var swipeGallery = {
+		v : {},
 
-var swipeGallery = {
-	v : {},
+		init : function(){
+			
+			var v = swipeGallery.v;
 
-	init : function(){
-		
-		var v = swipeGallery.v;
+			v.target = $('#gallery');
+			v.targetWidth = v.target.width();
+			v.targetHeight = v.target.height();
+			v.targetList = v.target.find('.galleryList');
+			v.targetItem = v.targetList.find('.item');
+			v.numNow = 0;
+			v.numTotal = v.targetItem.length-1;
+			v.nowItem = v.targetItem.eq(v.numNow);
+			v.targetItemPos = {};
 
-		v.target = $('#gallery');
-		v.targetWidth = v.target.width();
-		v.targetHeight = v.target.height();
-		v.targetList = v.target.find('.galleryList');
-		v.targetItem = v.targetList.find('.item');
-		v.numNow = 0;
-		v.numTotal = v.targetItem.length-1;
-		v.nowItem = v.targetItem.eq(v.numNow);
-		v.targetItemPos = {};
+			/* reset */
+			v.targetItem.css('left','-10000px');
+			v.targetItem.eq(v.numNow).css('left','0');
 
-		/* reset */
-		v.targetItem.css('left','-10000px');
-		v.targetItem.eq(v.numNow).css('left','0');
+			swipeGallery.regPos();
 
-		swipeGallery.regPos();
+			$(window).on('resize',swipeGallery.resize);
 
-		$(window).on('resize',swipeGallery.resize);
+			/*
+				$(해당 객체).swipeState > 현재 진행 중인지 출력 start,move,end
 
-		/*
-			$(해당 객체).swipeState > 현재 진행 중인지 출력 start,move,end
+				전달 인자 설명
+				returnstart : function(), // 터치,클릭 함수
+				returnmove : function(), // 이동 중 이벤트 함수
+				returnend : function(), // 터치,업 완료 함수
+				returncancel : function(), // 터치,클릭,이동 취소 함수, 최소 이동거리를 충족하지 못하고 mouseUP이 되는경우 취소 함수 실행, $(object).swipeCancel() < 이와 깉이 실행시 작동
+				minDistanceX : number // 최소로 이동해야하는 거리 (default:100px) 해당 수치만큼 이동하지 않을경우 returnend 실행에서 directionX나 directionY의 값은 stop 리턴
+				minDistanceY : number // 최소 이동해야하는 거리 y
 
-			전달 인자 설명
-			returnstart : function(), // 터치,클릭 함수
-			returnmove : function(), // 이동 중 이벤트 함수
-			returnend : function(), // 터치,업 완료 함수
-			returncancel : function(), // 터치,클릭,이동 취소 함수, 최소 이동거리를 충족하지 못하고 mouseUP이 되는경우 취소 함수 실행, $(object).swipeCancel() < 이와 깉이 실행시 작동
-			minDistanceX : number // 최소로 이동해야하는 거리 (default:100px) 해당 수치만큼 이동하지 않을경우 returnend 실행에서 directionX나 directionY의 값은 stop 리턴
-			minDistanceY : number // 최소 이동해야하는 거리 y
+			*/
+			v.target.swipe({
+				returnstart : swipeGallery.touchStart,
+				returnmove : swipeGallery.touchMove,
+				returnend : swipeGallery.touchEnd,
+				returncancel : swipeGallery.touchCancel,
+				minDistanceX : 100,
+				minDistanceY : 100
+			});
+		},
+		touchStart : function(e){
+			// console.log(e);
+			// console.log(this)
+			var v = swipeGallery.v;
 
-		*/
-		v.target.swipe({
-			returnstart : swipeGallery.touchStart,
-			returnmove : swipeGallery.touchMove,
-			returnend : swipeGallery.touchEnd,
-			returncancel : swipeGallery.touchCancel,
-			minDistanceX : 100,
-			minDistanceY : 100
-		});
-	},
-	touchStart : function(e){
-		// console.log(e);
-		// console.log(this)
-		var v = swipeGallery.v;
+			swipeGallery.regPos();
+			v.nowItem = $(v.targetItem.eq(v.numNow));
+		},
+		touchMove : function(e){
 
-		swipeGallery.regPos();
-		v.nowItem = $(v.targetItem.eq(v.numNow));
-	},
-	touchMove : function(e){
+			var v = swipeGallery.v,
+				posX = v.targetItemPos[v.numNow].x + e.distanceX;
 
-		var v = swipeGallery.v,
-			posX = v.targetItemPos[v.numNow].x + e.distanceX;
+			swipeGallery.drag({
+				left: posX - v.targetWidth,
+				now: posX,
+				right: posX + v.targetWidth
+			});
 
-		swipeGallery.drag({
-			left: posX - v.targetWidth,
-			now: posX,
-			right: posX + v.targetWidth
-		});
+		},
+		touchEnd : function(e){
+			var v = swipeGallery.v,
+				action;
+			console.log(e);
+			if( e.distanceX > 50 ){
+				action = e.directionX;
+				swipeGallery.move(action);
+			}else{
+				action = 'stop';
+			}
+		},
+		touchCancel : function(e){
+			//console.log(e);
+		},
+		regPos : function(){
+			var v = swipeGallery.v,
+				i;
 
-	},
-	touchEnd : function(e){
-		var v = swipeGallery.v,
-			action;
-		console.log(e);
-		if( e.distanceX > 50 ){
-			action = e.directionX;
-			swipeGallery.move(action);
-		}else{
-			action = 'stop';
+			for(i = 0; i < v.numTotal; i++){
+				v.targetItemPos[i] = {};
+				v.targetItemPos[i].x = v.targetItem.eq(i).position().left;
+				v.targetItemPos[i].y = v.targetItem.eq(i).position().top;
+			}
+		},
+		resize : function(){
+			var v = swipeGallery.v,
+				item = swipeGallery.viewItem();
+
+			v.targetWidth = v.target.width();
+			v.targetHeight = v.target.outerHeight();
+
+			swipeGallery.drag({
+				left: -1 * v.targetWidth,
+				now: 0,
+				right: v.targetWidth
+			});
+
+			swipeGallery.regPos();
+		},
+		drag : function(pos){
+			var v = swipeGallery.v,
+				item = swipeGallery.viewItem();
+
+			item.left.css('left', pos.left );
+			v.nowItem.css('left', pos.now );
+			item.right.css('left', pos.right );
+		},
+		move : function(action){
+			var v = swipeGallery.v,
+				item = swipeGallery.viewItem(),
+				pos = {},
+				animateOpt = {
+					duration: 200,
+					easing: 'easeOutCubic',
+					complete: animateComplete
+				};
+
+			switch (action) {
+				case 'left' :
+					pos.left = -1 * (v.targetWidth * 2);
+					pos.now = -1 * v.targetWidth;
+					pos.right = 0;
+					break;
+				case 'right' :
+					pos.left = 0;
+					pos.now = v.targetWidth;
+					pos.right = v.targetWidth * 2;
+					break;
+				case 'stop' :
+					pos.left = -1 * v.targetWidth;
+					pos.now = 0;
+					pos.right = v.targetWidth;
+					break;
+			}
+
+			item.left.animate({'left': pos.left},animateOpt);
+			v.nowItem.animate({'left': pos.now},animateOpt);
+			item.right.animate({'left': pos.right},animateOpt);
+
+			function animateComplete(){
+
+			}
+			v.numNow = ( action == 'left' ) ? item.right.index() : item.left.index();
+			//console.log(v.numNow)
+			v.nowItem = v.targetItem.eq(v.numNow);
+		},
+		viewItem : function(){
+			var v = swipeGallery.v,
+				returns = {};
+			returns.left = (v.numNow == 0) ? v.targetItem.eq(v.numTotal) : v.targetItem.eq(v.numNow - 1),
+			returns.right = (v.numNow == v.numTotal) ? v.targetItem.eq(0) : v.targetItem.eq(v.numNow + 1);
+
+			return returns;
+		},
+		defaultPos : function(){
+
 		}
-	},
-	touchCancel : function(e){
-		//console.log(e);
-	},
-	regPos : function(){
-		var v = swipeGallery.v,
-			i;
-
-		for(i = 0; i < v.numTotal; i++){
-			v.targetItemPos[i] = {};
-			v.targetItemPos[i].x = v.targetItem.eq(i).position().left;
-			v.targetItemPos[i].y = v.targetItem.eq(i).position().top;
-		}
-	},
-	resize : function(){
-		var v = swipeGallery.v,
-			item = swipeGallery.viewItem();
-
-		v.targetWidth = v.target.width();
-		v.targetHeight = v.target.outerHeight();
-
-		swipeGallery.drag({
-			left: -1 * v.targetWidth,
-			now: 0,
-			right: v.targetWidth
-		});
-
-		swipeGallery.regPos();
-	},
-	drag : function(pos){
-		var v = swipeGallery.v,
-			item = swipeGallery.viewItem();
-
-		item.left.css('left', pos.left );
-		v.nowItem.css('left', pos.now );
-		item.right.css('left', pos.right );
-	},
-	move : function(action){
-		var v = swipeGallery.v,
-			item = swipeGallery.viewItem(),
-			pos = {},
-			animateOpt = {
-				duration: 200,
-				easing: 'easeOutCubic',
-				complete: animateComplete
-			};
-
-		switch (action) {
-			case 'left' :
-				pos.left = -1 * (v.targetWidth * 2);
-				pos.now = -1 * v.targetWidth;
-				pos.right = 0;
-				break;
-			case 'right' :
-				pos.left = 0;
-				pos.now = v.targetWidth;
-				pos.right = v.targetWidth * 2;
-				break;
-			case 'stop' :
-				pos.left = -1 * v.targetWidth;
-				pos.now = 0;
-				pos.right = v.targetWidth;
-				break;
-		}
-
-		item.left.animate({'left': pos.left},animateOpt);
-		v.nowItem.animate({'left': pos.now},animateOpt);
-		item.right.animate({'left': pos.right},animateOpt);
-
-		function animateComplete(){
-
-		}
-		v.numNow = ( action == 'left' ) ? item.right.index() : item.left.index();
-		//console.log(v.numNow)
-		v.nowItem = v.targetItem.eq(v.numNow);
-	},
-	viewItem : function(){
-		var v = swipeGallery.v,
-			returns = {};
-		returns.left = (v.numNow == 0) ? v.targetItem.eq(v.numTotal) : v.targetItem.eq(v.numNow - 1),
-		returns.right = (v.numNow == v.numTotal) ? v.targetItem.eq(0) : v.targetItem.eq(v.numNow + 1);
-
-		return returns;
-	},
-	defaultPos : function(){
-
 	}
-}
 
 
-$(document).ready(function(){
-	swipeGallery.init( $('#gallery') );
-});
+	$(document).ready(function(){
+		swipeGallery.init( $('#gallery') );
+	});
+
+})();
 
 
 
